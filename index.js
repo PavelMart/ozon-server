c1ab7f613b1b346f28af7c15d4bb06735392d33a
PavelMart <martackovp@yandex.ru>
2023-03-21 22:08:04 +0300

    version 1.3.4

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middleware/errorMiddlware");
 const { Product } = require("./models/product.model");
 const { ApiKey } = require("./models/api.model");
+const ApiError = require("./api/ApiError");
 
 dotenv.config();
 
@@ -24,7 +25,13 @@ app.use(express.static("public"));
 
 app.use(fileUpload({}));
 
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "index.html")));
+app.get("/", (req, res, next) => {
+  try {
+    res.sendFile(path.resolve(__dirname, "index.html"));
+  } catch (error) {
+    return next(ApiError.BadRequest(`getHtml: ${error.message}`));
+  }
+});
 
 app.use("/api", router);
 
